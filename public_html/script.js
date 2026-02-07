b00e38026cee45ebbd9a2a9ef2eb823ac521b349
Nicolas Sirghii <nicolas.mailbox100@gmail.com>
2026-02-07 16:01:45 +0200

    added loading

--- public_html/script.js
+++ public_html/script.js
@@ -1,11 +1,34 @@
- async function getUsers() {
-  try {
-    const response = await fetch("https://python-mysql-api.onrender.com");
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error("Error:", error);
-  }
+const loader = document.getElementById("loader");
+const content = document.getElementById("content");
+
+// async function that loads data
+async function loadData() {
+    try {
+        
+        const response = await fetch("https://python-mysql-api.onrender.com"); // your API endpoint
+        const data = await response.json();
+
+
+        // hide loader when request is finished
+        loader.style.display = "none";
+        content.style.display = "flex";
+        //.......................................................................................................
+        let con = ''
+         data.forEach(element => {
+            con += `<button class = "tables">${element}</button>`
+         });
+
+
+
+
+        content.innerHTML = con
+        //.......................................................................................................
+
+    } catch (error) {
+        loader.innerHTML = "<p>❌ Failed to load data</p>";
+    }
 }
 
-getUsers();
+// start loading
+loadData();
+
